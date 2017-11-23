88e4d03205c7185d2bf1e41c1433651551bc3c21
Clayton Lautier <claylaut@hotmail.com>
2017-11-23 23:38:24 +0100

    refactor(core extensions): refactor + split extensions

--- src/SignalR.Orleans/Extensions.cs
+++ src/SignalR.Orleans/Extensions.cs
@@ -7,7 +7,7 @@ using SignalR.Orleans.Clients;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class OrleansDependencyInjectionExtensions
+    public static class OrleansServerExtensions
     {
         public static ClusterConfiguration AddSignalR(this ClusterConfiguration config)
         {
@@ -24,35 +24,25 @@ namespace Microsoft.Extensions.DependencyInjection
             return config;
         }
 
+        public static ISiloHostBuilder UseSignalR(this ISiloHostBuilder builder)
+        {
+            return builder
+                .AddApplicationPartsFromReferences(typeof(ClientGrain).Assembly);
+        }
+    }
+
+    public static class OrleansClientExtensions
+    {
         public static ClientConfiguration AddSignalR(this ClientConfiguration config)
         {
             config.AddSimpleMessageStreamProvider(Constants.STREAM_PROVIDER);
             return config;
         }
 
-        public static ISiloHostBuilder UseSignalR(this ISiloHostBuilder builder)
-        {
-            return builder
-                .AddApplicationPartsFromReferences(typeof(ClientGrain).Assembly);
-        }
-
         public static IClientBuilder UseSignalR(this IClientBuilder builder)
         {
             return builder
                 .AddApplicationPartsFromReferences(typeof(IClientGrain).Assembly);
-        }
-
-        public static ISignalRBuilder AddOrleans(this ISignalRBuilder builder, IClientBuilder clientBuilder)
-        {
-            var client = clientBuilder.Build();
-            client.Connect().Wait();
-            return builder.AddOrleans(client);
-        }
-
-        public static ISignalRBuilder AddOrleans(this ISignalRBuilder builder, IClusterClient clusterClient)
-        {
-            builder.Services.AddSingleton(typeof(IClusterClient), clusterClient);
-            return builder.AddOrleans();
         }
 
         public static ISignalRBuilder AddOrleans(this ISignalRBuilder builder)
