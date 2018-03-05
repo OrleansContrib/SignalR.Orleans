ec8fc12f85d8a9c694df27956b70ccd2b3e25a92
Jonathan Sant <jonathan.sant@live.co.uk>
2018-03-05 16:34:52 +0100

    feat(all): Allow `SimpleMessageStreamProvider` to be configured to use fire-and-forget delivery. (#26)

--- src/SignalR.Orleans/Extensions.cs
+++ src/SignalR.Orleans/Extensions.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using Microsoft.AspNetCore.SignalR;
 using Orleans;
 using Orleans.Hosting;
@@ -9,22 +8,22 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class OrleansServerExtensions
     {
-        public static ISiloHostBuilder UseSignalR(this ISiloHostBuilder builder)
+        public static ISiloHostBuilder UseSignalR(this ISiloHostBuilder builder, bool useFireAndForgetDelivery = false)
         {
             try { builder = builder.AddMemoryGrainStorage("PubSubStore"); }
             catch { /** PubSubStore was already added. Do nothing. **/ }
 
             return builder.AddMemoryGrainStorage(Constants.STORAGE_PROVIDER)
-                .AddSimpleMessageStreamProvider(Constants.STREAM_PROVIDER)
+                .AddSimpleMessageStreamProvider(Constants.STREAM_PROVIDER, opt => opt.FireAndForgetDelivery = useFireAndForgetDelivery)
                 .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ClientGrain).Assembly).WithReferences());
         }
     }
 
     public static class OrleansClientExtensions
     {
-        public static IClientBuilder UseSignalR(this IClientBuilder builder)
+        public static IClientBuilder UseSignalR(this IClientBuilder builder, bool useFireAndForgetDelivery = false)
         {
-            return builder.AddSimpleMessageStreamProvider(Constants.STREAM_PROVIDER)
+            return builder.AddSimpleMessageStreamProvider(Constants.STREAM_PROVIDER, opt => opt.FireAndForgetDelivery = useFireAndForgetDelivery)
                 .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IClientGrain).Assembly).WithReferences())
                 .ConfigureServices(services => services.AddSingleton(typeof(HubLifetimeManager<>), typeof(OrleansHubLifetimeManager<>)));
         }
