66ca0aa8395dd54e05f50ee93d076aa6ddc5fb61
Gutemberg Ribeiro <gutemberg@outlook.com>
2018-06-23 01:53:45 -0300

    Deffer internal stream initialization
    This allow this package to be used in .Net Generic Host

--- test/SignalR.Orleans.Tests/OrleansFixture.cs
+++ test/SignalR.Orleans.Tests/OrleansFixture.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
-using Orleans.Runtime;
 
 namespace SignalR.Orleans.Tests
 {
@@ -15,22 +14,17 @@ namespace SignalR.Orleans.Tests
 
         public OrleansFixture()
         {
-            var siloPort = 11111;
-            int gatewayPort = 30000;
-            var siloAddress = IPAddress.Loopback;
-
             var silo = new SiloHostBuilder()
-                .Configure<ClusterOptions>(options => options.ClusterId = "test-cluster")
-                .UseDevelopmentClustering(options => options.PrimarySiloEndpoint = new IPEndPoint(siloAddress, siloPort))
-                .ConfigureEndpoints(siloAddress, siloPort, gatewayPort)
+                .UseLocalhostClustering()
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
                 .UseSignalR()
                 .Build();
             silo.StartAsync().Wait();
             this.Silo = silo;
 
             var client = new ClientBuilder()
-                .Configure<ClusterOptions>(options => options.ClusterId = "test-cluster")
-                .UseStaticClustering(options => options.Gateways.Add(new IPEndPoint(siloAddress, gatewayPort).ToGatewayUri()))
+                .UseLocalhostClustering()
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
                 .UseSignalR()
                 .Build();
 
