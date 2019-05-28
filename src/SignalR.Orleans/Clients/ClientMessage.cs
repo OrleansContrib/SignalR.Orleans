f541ccc88ae35a315e589eaefd21db497a19d2ea
Stephen Lautier <stephen.lautier@outlook.com>
2019-05-28 17:59:04 +0200

    remove todo

--- src/SignalR.Orleans/Clients/ClientMessage.cs
+++ src/SignalR.Orleans/Clients/ClientMessage.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.SignalR.Protocol;
 
 namespace SignalR.Orleans.Clients
 {
-    // todo: debugger display
     public class ClientMessage
     {
         public string HubName { get; set; }
