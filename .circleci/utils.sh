e6e8137089ce38a930ef177371fd87be9b09efcf
Clayton Lautier <clayton.lautier@outlook.com>
2023-06-09 16:51:36 +0200

    chore(ci): reuseable workflow (#31)
    Co-authored-by: Stephen Lautier <stephen.lautier@outlook.com>

--- .circleci/utils.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-print_title() {
-	echo -e "\e[36m ---- $1 ---- \e[39m"
-}
--- tools/version-builder.sh
+++ tools/version-builder.sh
@@ -3,13 +3,22 @@
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 PACKAGE_VERSION_SUFFIX=$(node -p "require('./package.json').versionSuffix")
 VERSION=$PACKAGE_VERSION
+# BRANCH=$CIRCLE_BRANCH
+BRANCH=$GITHUB_REF_NAME
+
+# BUILD_NUM=$CIRCLE_BUILD_NUM
+BUILD_NUM=$GITHUB_RUN_NUMBER
+
+echo "BRANCH: $BRANCH, BUILD_NUM: $BUILD_NUM"
 
 if [ -z "$PACKAGE_VERSION_SUFFIX" ] && [ -z "$CI" ]; then
 	PACKAGE_VERSION_SUFFIX=dev
 fi
 
 if [ $CIRCLE_BRANCH = "master" ] ; then
-	PACKAGE_VERSION_SUFFIX=dev$CIRCLE_BUILD_NUM
+	PACKAGE_VERSION_SUFFIX=dev$BUILD_NUM
+elif [ -n $BRANCH ]; then
+	echo BRANCH EMPTY
 fi
 
 if [ -n "$PACKAGE_VERSION_SUFFIX" ]; then
